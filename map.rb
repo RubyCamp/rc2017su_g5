b2fa599dc680e22c4e1eed97058c0fda09b2e003
ririnoia <riguaru0127@gmail.com>
2017-08-23 09:42:23 +0900

    Add map,map_display

--- map.rb
+++ map.rb
@@ -13,20 +13,20 @@ class Map
     map1 = Mapfile.new(1)
     arr = Array.new(100).map{Array.new(60,0)}
     arr=map1.map
+    objects=[]
     space = CP::Space.new
 
 
     60.times do |i|
       100.times do |j|
         num=arr[i][j]
-        objects=[]
         case num
         when 0 then
           #null
         when 1 then
           objects << Wall.new(i*10, j*10 ,space)
         when 2 then
-          #goal
+          objects << Goal.new(i*10, j*10 ,space)
         end
       end
     end
@@ -34,5 +34,9 @@ class Map
 
 end
 
-map = Map.new
-map.put
+Window.loop do
+  space.step(speed)
+  objects.each do |obj|
+    obj.draw
+  end
+end
--- map_display.rb
+++ map_display.rb
@@ -4,7 +4,7 @@ require 'chipmunk'
 class Wall
   def initialize(x, y, space)
     @x, @y = x, y
-    @image = Image.new(10, 10, C_WHITE)
+    @image = Image.new(10, 10, C_WHITE) #画像を読み込む形に差し替えお願いします
     @body = CP::StaticBody.new
     @body.p = CP::Vec2.new(0, 0)
     verts = [
@@ -18,8 +18,23 @@ class Wall
     @shape.u = 0.0
     @shape.add_to_space(space)
   end
+end
 
-  def draw
-    Window.draw(@x, @y, @image)
+class Goal
+  def initialize()
+    @x, @y = x, y
+    @image = Image.new(10, 10, C_RED) #画像を読み込む形に差し替えお願いします
+    @body = CP::StaticBody.new
+    @body.p = CP::Vec2.new(0, 0)
+    verts = [
+      CP::Vec2.new(x, y),         # 第１頂点（左上）
+      CP::Vec2.new(x, y + 10),     # 第２頂点（左下）
+      CP::Vec2.new(x + 10, y + 10), # 第３頂点（右下）
+      CP::Vec2.new(x + 10, y)      # 第４頂点（右上）
+    ]
+    @shape = CP::Shape::Poly.new(@body, verts, CP::Vec2.new(0, 0))
+    @shape.e = 1.0
+    @shape.u = 0.0
+    @shape.add_to_space(space)
   end
 end
