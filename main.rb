476af0e0c3aeae41a57ba3d6b27eff74a1013f3e
shiotomo <t.shiota16@gmail.com>
2017-08-24 09:51:36 +0900

    Add time

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'dxruby'
 require 'chipmunk'
+require 'benchmark'
 
 require_relative 'filecounter'
 require_relative 'map'
@@ -11,8 +12,6 @@ Window.width = 1000
 Window.height = 600
 space = CP::Space.new
 space.gravity = CP::Vec2.new(0, 100)
-speed=1/60.0
-objects=[]
 
 map = Map.new(space)
 
@@ -26,17 +25,21 @@ end
 
 number = course.to_i
 
-0.upto(number + 5) do |num|
-  p num + 1
-  map.put(num)
-  map.delete
-end
+result = Benchmark.measure {
+  0.upto(number + 5) do |num|
+    p num + 1
+    map.put(num)
+    map.delete
+  end
+}
 
+aaa = "結果発表!!"
+puts result.real
 
 # 結果発表
 Window.loop do
 
   break if Input.key_push?(K_ESCAPE)
-  Window.draw_font(500, 280, "結果発表", font)
+  Window.draw_font(500, 280, aaa, font)
 
 end
